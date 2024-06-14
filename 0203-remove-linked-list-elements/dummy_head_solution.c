93bdf3ba0826d0e23d3895b43f027c479c1572d8
Jack Lin <blueskyson1401@gmail.com>
2024-06-14 13:51:27 +0800

    Create dummy_head_solution.c

---
+++ 0203-remove-linked-list-elements/dummy_head_solution.c
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    typedef struct ListNode ListNode;
+    ListNode *dummy = (ListNode*)malloc(sizeof(ListNode));
+    dummy->next = head;
+    ListNode *current = head;
+    while (current != NULL) {
+        if (current->val == val) {
+            if (current == head) {
+                head = current->next;
+            }
+            dummy->next = current->next;
+            current = current->next;
+        } else {
+            dummy = current;
+            current = current->next;
+        }
+    }
+
+    return head;
+}
