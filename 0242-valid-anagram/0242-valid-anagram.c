4740aa452f394e7aca39b38a10ec308eba07170c
Jack Lin <blueskyson1401@gmail.com>
2024-06-15 14:56:51 +0800

    Time: 0 ms (100.00%), Space: 6.1 MB (25.90%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.c
@@ -0,0 +1,27 @@
+bool isAnagram(char* s, char* t) {
+    int len1 = strlen(s), len2 = strlen(t);
+    if (len1 != len2) {
+        return false;
+    }
+
+    int map1[26], map2[26];
+    for (int i = 0; i < 26; i++) {
+        map1[i] = 0;
+        map2[i] = 0;
+    }
+
+    for (int i = 0; i < len1; i++) {
+        int c1 = (int)(s[i] - 'a');
+        int c2 = (int)(t[i] - 'a');
+        map1[c1] += 1;
+        map2[c2] += 1;
+    }
+    
+    for (int i = 0; i < 26; i++) {
+        if (map1[i] != map2[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
