4921e2b5f515ccd9491817d083af8671521602b5
Jack Lin <blueskyson1401@gmail.com>
2024-06-26 22:11:12 +0800

    Time: 3 ms (41.64%), Space: 8.1 MB (16.17%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push(')');
+            } else if (s[i] == '{') {
+                st.push('}');
+            } else if (s[i] == '[') {
+                st.push(']');
+            } else if (!st.empty() && s[i] == st.top()) {
+                st.pop();
+            } else {
+                return false;
+            }
+        }
+        
+        return st.empty();
+    }
+};
