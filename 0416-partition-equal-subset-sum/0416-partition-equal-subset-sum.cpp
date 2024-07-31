eacda3b36b73f8bb2e5d3ef36413b018f8668df9
Jack Lin <blueskyson1401@gmail.com>
2024-08-01 04:31:15 +0800

    Time: 58 ms (90.21%), Space: 19.6 MB (59.13%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        vector<int> dp(10001, 0);
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+
+        if (sum % 2 == 1)
+            return false;
+
+        int half = sum / 2;        
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = half; j >= nums[i]; j--) {
+                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+        
+        return dp[half] == half;
+    }
+};
