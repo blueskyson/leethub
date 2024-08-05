50f17bb2ec7b1ffd694bb2d2ed5e245e2c4028d0
Jack Lin <blueskyson1401@gmail.com>
2024-08-05 19:52:57 +0800

    Time: 6 ms (75.79%), Space: 11.3 MB (67.01%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+
+        if (abs(target) > sum || (sum + target) % 2 == 1) {
+            return 0;
+        }
+
+        int bagSize = (sum + target) / 2;
+        vector<int> dp(bagSize + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = bagSize; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+
+        return dp[bagSize];
+    }
+};
