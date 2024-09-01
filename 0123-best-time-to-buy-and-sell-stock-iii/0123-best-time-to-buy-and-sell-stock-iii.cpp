cc26d0ba5b0a891ece21a0b844b409941d0a0156
Jack Lin <blueskyson1401@gmail.com>
2024-09-01 16:01:32 -0700

    Time: 183 ms (68.27%), Space: 126 MB (67.07%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() == 1) {
+            return 0;
+        }
+        vector<vector<int>> dp(prices.size(), vector<int>(4, 0));
+        dp[0][0] = -prices[0];
+        dp[0][2] = -prices[0];
+        for (int i = 1; i < prices.size(); i++) {
+            dp[i][0] = max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1] - prices[i]);
+            dp[i][3] = max(dp[i - 1][3], dp[i - 1][2] + prices[i]);
+        }
+        
+        return max(dp[prices.size() - 1][1], dp[prices.size() - 1][3]);
+    }
+};
