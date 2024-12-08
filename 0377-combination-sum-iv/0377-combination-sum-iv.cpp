eac4d7f1f8aebe013043ac7ec83c27d51cfe08fc
Jack Lin <blueskyson1401@gmail.com>
2024-12-07 23:12:31 -0800

    Time: 0 ms (100.00%), Space: 9.1 MB (20.03%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned int> dp(target + 1, 0);
+        vector<unsigned> dp(target + 1, 0);
         dp[0] = 1;
-        for (int i = 0; i <= target; i++) {
+        for (int i = 1; i <= target; i++) {
             for (int j = 0; j < nums.size(); j++) {
                 if (i >= nums[j]) {
                     dp[i] += dp[i - nums[j]];
                 }
             }
         }
+        
+        // for (int i = 0; i < dp.size(); i++) {
+        //     cout << dp[i] << " ";
+        // }
+        // cout << endl;
         return dp[target];
     }
 };
