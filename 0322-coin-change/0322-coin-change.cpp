292e7708209b8384b4a9de4d96ef5a90c8ba9858
Jack Lin <blueskyson1401@gmail.com>
2024-12-07 21:41:37 -0800

    Time: 15 ms (93.01%), Space: 18 MB (49.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<unsigned int> dp(amount + 1, INT32_MAX);
+        vector<unsigned> dp(amount + 1, INT32_MAX);
         dp[0] = 0;
         for (int i = 0; i < coins.size(); i++) {
             for (int j = coins[i]; j <= amount; j++) {
@@ -12,6 +12,7 @@ public:
         if (dp[amount] == INT32_MAX) {
             return -1;
         }
+        
         return dp[amount];
     }
 };
