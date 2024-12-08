c7f62c392ac71f45560a221d102848f4dda4b8cc
Jack Lin <blueskyson1401@gmail.com>
2024-12-07 19:34:12 -0800

    Time: 0 ms (100.00%), Space: 8.7 MB (5.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,15 +4,14 @@ public:
         if (n == 1) {
             return 1;
         }
-
-        vector<int> dp(n + 1, 0);
+        
+        vector<int> dp(n, 0);
         dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] += dp[i - 1];
-            dp[i] += dp[i - 2];
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         
-        return dp[n];
+        return dp[n - 1];
     }
 };
